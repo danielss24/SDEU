c8c79c35d41f5e3db94fa17eb28f09595fda8a9e
Juan A. Escalante <jaescalante02@gmail.com>
2020-03-13 19:16:09 +0100

    Carrito chocón.

--- robot/Proyecto/main.cpp
+++ robot/Proyecto/main.cpp
@@ -38,10 +38,19 @@ int jail_rotating(Component *holobot);
 int get_danger_component(Component *holobot);
 int calibrator(Component *holobot);
 int in(float last, float current);
+void update_initial(Component *holobot);
+
 
 float speedTest = 0.3f;
+
 float AJUSTE = 0.03f;
-float AJUSTE_GIRO = 0.08f;
+float AJUSTE_STEP = 0.0025f;
+float MIN_AJUSTE = 0.01f + AJUSTE_STEP;
+float MAX_AJUSTE = 0.035f - AJUSTE_STEP;
+
+float DEFAULT_AJUSTE_GIRO = 0.04f;
+float AJUSTE_GIRO = DEFAULT_AJUSTE_GIRO;
+
 float MOVEMENT_TIME = 0.005f;
 
 int main() {
@@ -110,25 +119,35 @@ int main() {
     holobot[2].initial = a3.read();
     holobot[2].current = holobot[2].initial;
 
-    if ((in(holobot[0].current, holobot[1].current)) && (in(holobot[0].current, holobot[2].current))){
-        myled4 = 1;
-    }
+    // if ((in(holobot[0].current, holobot[1].current)) && (in(holobot[0].current, holobot[2].current))){
+    //     myled4 = 1;
+    // }
 
     while(1) {
 
         update_measures(holobot);
 
-        if (equal(holobot)) continue;
+        if (equal(holobot)){
+            myled1 = !myled1;
+            if (AJUSTE > MIN_AJUSTE) AJUSTE = AJUSTE - AJUSTE_STEP;
+            continue;
+        }
 
-        myled1 = 1;
+        int danger_component = get_danger_component(holobot);
+        if (danger_component){      
+            mantener_distancia(danger_component, holobot);
+            wait(MOVEMENT_TIME);
+            parar_motores(holobot);
+        }
+
+        update_measures(holobot);
 
         if (jail_rotating(holobot)){
             //logica rotar
             AJUSTE_GIRO = 0.02f;
             float current, attempt;
-            float initial = holobot[0].initial + holobot[1].initial + holobot[2].initial;
             int mode = 1;
-            myled2 = 1;
+            myled3 = !myled3;
 
             while (jail_rotating(holobot)) {
                 current = holobot[0].current + holobot[1].current + holobot[2].current;
@@ -140,24 +159,17 @@ int main() {
                 update_measures(holobot);
                 attempt = holobot[0].current + holobot[1].current + holobot[2].current;
 
-                if (attempt < current) {
-                    mode = !mode;
-                    parar_motores(holobot);
-                    myled3 = 1;
-                }
+                if ( mode && (attempt < current)) mode = !mode;
                 update_measures(holobot);
             }
             parar_motores(holobot);
-            AJUSTE_GIRO = 0.08f;
-        }
+            AJUSTE_GIRO = DEFAULT_AJUSTE_GIRO;
+        }       
 
-        int danger_component = get_danger_component(holobot);
-        if (danger_component) {
-            mantener_distancia(danger_component, holobot);
-            wait(MOVEMENT_TIME);
-            myled4 = 1;
+        if (AJUSTE < MAX_AJUSTE) {
+            myled2 = !myled2;
+            AJUSTE = AJUSTE + AJUSTE_STEP;
         }
-        parar_motores(holobot);
     }
 }
 
@@ -204,6 +216,33 @@ int equal(Component *holobot){
 
 }
 
+void update_initial(Component *holobot){
+
+    float avg = (holobot[0].initial + holobot[1].initial + holobot[2].initial)/3.0f;
+
+    float errin0 = avg - holobot[0].initial;
+    if (errin0 < 0) errin0 = - errin0;
+    float errin1 = avg - holobot[1].initial;
+    if (errin1 < 0) errin1 = - errin1;
+    float errin2 = avg - holobot[2].initial;
+    if (errin2 < 0) errin2 = - errin2;
+
+    float errcu0 = avg - holobot[0].current;
+    if (errcu0 < 0) errcu0 = - errcu0;
+    float errcu1 = avg - holobot[1].current;
+    if (errcu1 < 0) errcu1 = - errcu1;
+    float errcu2 = avg - holobot[2].current;
+    if (errcu2 < 0) errcu2 = - errcu2;
+
+    if ((errcu0 < errin0) && (errcu1 < errin1) && (errcu2 < errin2)){
+        holobot[0].initial = holobot[0].current;
+        holobot[1].initial = holobot[1].current;
+        holobot[2].initial = holobot[2].current;
+        myled4 = !myled4;
+    }
+
+}
+
 int get_danger_component(Component *holobot){
 
     float margen0 = holobot[0].initial + holobot[0].initial * AJUSTE;
@@ -218,7 +257,21 @@ int get_danger_component(Component *holobot){
     if ((diff0 < diff1) && (diff0 < diff2) && (diff0 < 0)) return 1;
     if ((diff1 < diff0) && (diff1 < diff2) && (diff1 < 0)) return 2;
     if ((diff2 < diff0) && (diff2 < diff1) && (diff2 < 0)) return 3;
+
     return 0;
+
+    margen0 = holobot[0].initial - holobot[0].initial * AJUSTE;
+    diff0 = margen0 - holobot[0].current;
+
+    margen1 = holobot[1].initial - holobot[1].initial * AJUSTE;
+    diff1 = margen1 - holobot[1].current;
+
+    margen2 = holobot[2].initial - holobot[2].initial * AJUSTE;
+    diff2 = margen2 - holobot[2].current;
+
+    if ((diff1 < diff0) && (diff2 < diff0) && (0 < diff0)) return 1;
+    if ((diff0 < diff1) && (diff2 < diff1) && (0 < diff1)) return 2;
+    if ((diff0 < diff2) && (diff1 < diff2) && (0 < diff2)) return 3;
 
 }
 
@@ -260,6 +313,7 @@ void update_measures(Component *holobot){
     holobot[1].current = holobot[1].a->read();
     holobot[2].current = holobot[2].a->read();
 
+    update_initial(holobot);
 }
 
 void mover_horario(Component *component) {
@@ -303,23 +357,58 @@ void modificar_velocidad_h(Component *component, float speed){
 }
 
 void mantener_distancia(int sensor, Component *holobot){
-
+                            
+    float margenSup = 0.0f;
+    float margenInf = 0.0f;
+    
     if (sensor == 1) {
-
-        mover_horario(&holobot[1]);
-        mover_anti_horario(&holobot[2]);
-
+        margenSup = holobot[0].initial + holobot[0].initial * AJUSTE;
+        margenInf = holobot[0].initial - holobot[0].initial * AJUSTE;
+        if (holobot[0].current > margenSup){
+            // Esta mas cerca
+            mover_horario(&holobot[1]);
+            mover_anti_horario(&holobot[2]);
+            
+        } else if (holobot[0].current < margenInf ) {
+            // Esta mas lejos
+            mover_horario(&holobot[2]);
+            mover_anti_horario(&holobot[1]);      
+        } else {
+            parar_motor(&holobot[1]);
+            parar_motor(&holobot[2]);
+        }
     } else if (sensor == 2) {
-
-        mover_horario(&holobot[2]);
-        mover_anti_horario(&holobot[0]);
-
+        margenSup = holobot[1].initial + holobot[1].initial * AJUSTE;
+        margenInf = holobot[1].initial - holobot[1].initial * AJUSTE;
+        if (holobot[1].current > margenSup){
+            // Esta mas cerca
+            mover_horario(&holobot[2]);
+            mover_anti_horario(&holobot[0]);
+        } else if (holobot[1].current < margenInf ) {
+            // Esta mas lejos
+            mover_horario(&holobot[0]);
+            mover_anti_horario(&holobot[2]);      
+        } else {
+            parar_motor(&holobot[0]);
+            parar_motor(&holobot[2]);   
+        }
     } else if (sensor == 3) {
-
-        mover_horario(&holobot[0]);
-        mover_anti_horario(&holobot[1]);
-
+        margenSup = holobot[2].initial + holobot[2].initial * AJUSTE;
+        margenInf = holobot[2].initial - holobot[2].initial * AJUSTE;
+        if (holobot[2].current > margenSup){
+            // Esta mas cerca
+            mover_horario(&holobot[0]);
+            mover_anti_horario(&holobot[1]);
+        } else if (holobot[2].current < margenInf ) {
+            // Esta mas lejos
+            mover_horario(&holobot[1]);
+            mover_anti_horario(&holobot[0]);    
+        } else {
+            parar_motor(&holobot[1]);
+            parar_motor(&holobot[0]);
+        }
     }
+    
 }
 
 void rotar_horario(Component *holobot){
